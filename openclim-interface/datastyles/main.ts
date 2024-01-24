5e506501214044f0ce4a68a8cfd019051ce041d6
Craig Robson <craig.robson1@ncl.ac.uk>
2024-01-24 12:53:38 +0000

    added simple data style

---
+++ openclim-interface/datastyles/main.ts
@@ -0,0 +1,63 @@
+export const unavailableStyle = {
+    fillColor: "grey",
+    weight: 1,
+    opacity: 1,
+    color: 'black',
+    fillOpacity: 0.8
+};
+
+export const defaultStyle = {
+    fillColor: "green",
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 0.4
+};
+
+export const availableStyle = {
+    fillColor: "orange",
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 0.2
+};
+
+export const grassStyle = {
+    fillColor: "green",
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 0.2
+};
+
+export const population = {
+    fillColor: "green",
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 0.3
+};
+
+export const avgHeat = {
+    fillColor: "red",
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 0.3
+};
+
+export const minMort = {
+    fillColor: "orange",
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 0.3
+}
+
+export const relativeRisk = {
+    fillColor: "purple",
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    fillOpacity: 0.3
+}
